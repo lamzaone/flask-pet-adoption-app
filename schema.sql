fb2e2813797b86141af79185f24c63cbf5121e85
lamzaone <denisdenis009@gmail.com>
2024-01-14 07:00:25 +0200

    updated schema.sql to the latest db model

--- schema.sql
+++ schema.sql
@@ -1,20 +1,50 @@
-CREATE TABLE user (
-  id TEXT PRIMARY KEY,
-  name TEXT NOT NULL,
-  email TEXT UNIQUE NOT NULL,
-  profile_pic TEXT NOT NULL
+create table main.user
+(
+    id          TEXT
+        primary key,
+    name        TEXT                  not null,
+    email       TEXT                  not null
+        unique,
+    profile_pic TEXT                  not null,
+    is_admin    boolean default false null on conflict replace
 );
 
 
-CREATE TABLE animals (
-  id TEXT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  name TEXT NOT NULL,
-  user_id TEXT,
-  kind TEXT NOT NULL,
-  breed TEXT,
-  sex TEXT,
-  age TEXT,
-  description TEXT,
-  img_url TEXT NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES user(id)
+
+
+create table main.animals
+(
+    id          INTEGER not null
+        primary key autoincrement ,
+    name        TEXT    not null,
+    user_id     TEXT
+        references main.user,
+    kind        TEXT    not null,
+    breed       TEXT,
+    sex         TEXT,
+    age         TEXT,
+    description TEXT,
+    img_url     TEXT    not null,
+    location    TEXT,
+    adopted     boolean default 0
 );
+
+
+create table main.adoption_forms
+(
+    user_id             TEXT
+        references main.user,
+    animal_id           TEXT
+        references main.animals,
+    owner_id            TEXT
+        references main.user,
+    user_age            TEXT,
+    location            TEXT,
+    building_type       TEXT,
+    current_pets        TEXT,
+    previous_experience TEXT,
+    phone_number        TEXT,
+    status              TEXT default "pending",
+    primary key (user_id, animal_id)
+);
+
